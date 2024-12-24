32530b14f015c79e861ac6365b911c476be819ba
Ashish Kumar Mali <kumarmaliashish0@gmail.com>
2024-12-24 20:53:57 +0530

    Add files via upload

--- color_changer.js
+++ color_changer.js
@@ -1,6 +1,5 @@
 const root = document.getElementById('root')
 
 root.addEventListener('click',(event)=>{
-    console.log(event.target.classname);
     document.body.style.backgroundColor = event.target.id;
 })
